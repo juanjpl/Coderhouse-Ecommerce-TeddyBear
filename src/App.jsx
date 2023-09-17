9dded0e25ec76620218e5d56d4ae1b8d26943f44
Juan Lima <juan_jpl@hotmail.com>
2023-09-17 15:34:42 -0300

    update 4.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
-import { useState ,useEffect } from 'react'
-import CardUser from './components/CardUser.jsx';
-import './App.css'
+import { useState, useEffect } from "react";
+import CardUser from "./components/CardUser.jsx";
+import "./App.css";
 
-const App =()=> {
+//Firebase
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "./firebase/firebaseConfig.js";
 
+const App = () => {
+  const [bears,setBears] = useState([]);
+  useEffect(() => {
+   
+    const getBears = async () => {
+      const q = query(collection(db, "teddy-bears"));
+      const docs=[];
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+
+        docs.push({...doc.data(),id: doc.id})
+      });
+
+      setBears(docs);
+    };
+
+    getBears();
+  }, []);
 
   return (
     <>
-  <h1>Firebase</h1>
-
+      <h1>Teddy Bears Shop</h1>
+      <div className="grid">
+      {bears.map((bear)=>{
+        return(
+          <div key={bear.id}>
+            <CardUser data={bear}/>
+          </div>
+        )
+      })}
+      </div>
+     
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
