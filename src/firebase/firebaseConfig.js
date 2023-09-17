1ae746dd004026fe3fd44b768f8f706f711cbfc8
Juan Lima <juan_jpl@hotmail.com>
2023-09-17 01:24:54 -0300

    first commit

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBeBI_RX_h1YOq2OWDCZWtxX8-vmGvSQ9I",
+  authDomain: "fb-58058-teddy-bears.firebaseapp.com",
+  projectId: "fb-58058-teddy-bears",
+  storageBucket: "fb-58058-teddy-bears.appspot.com",
+  messagingSenderId: "231890802909",
+  appId: "1:231890802909:web:29368cf85d77cb5aca6334"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
