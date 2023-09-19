4ee32d3a6f5e5fee098e0a53cc797f39ac4aef18
Juan Lima <juan_jplhotmail.com>
2023-09-19 16:21:39 -0300

    dev-one 1.0.0

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,19 @@ import styles from './styles.css';
 const Navbar =()=>{
     return(
         <div>
+            <div className="barra-social">
+                <div className='icons-redes'>
+
+                </div>
+                <div className="icons-texto">
+
+                </div>
+            </div>
+            <div className="navbar">
+                <div className="logo-teddy">
+                    
+                </div>
+            </div>
             <h1>Navbar</h1>
         </div>
     )
