e286b87ad449c38d26601411270acc47e3fbb48a
Juan Lima <juan_jplhotmail.com>
2023-09-20 14:25:33 -0300

    update option payment

--- src/components/BannerOne/BannerOne.jsx
+++ src/components/BannerOne/BannerOne.jsx
@@ -4,13 +4,33 @@ import   './styles.css';
 const BannerOne =()=>{
     return(
         <div className='banner-one'>
-            <h2>hola. soy el bannerone</h2>
-            <i class="fa-brands fa-square-facebook"></i>
-            <i class="fa-brands fa-instagram"></i>
-            <i class="fa-brands fa-x-twitter"></i>
-            <i class="fa-brands fa-pinterest-p"></i>
-            <i class="fa-brands fa-linkedin"></i>
-            <i class="fa-solid fa-share-nodes"></i>
+        
+            <div className="option-shop">
+                <div className="option">
+                <i class="fa-solid fa-plane"></i>
+                    <h2>FREE SHIPPING
+                    </h2>
+                    <h3>Free Shipping on all US order</h3>               
+                     </div>
+                     <div className="option">
+                     <i class="fa-solid fa-comments"></i>
+                    <h2>SUPPORT 24/7
+                    </h2>
+                    <h3>    Let us know how we can help</h3>               
+                     </div>
+                     <div className="option">
+                     <i class="fa-solid fa-dollar-sign"></i>
+                    <h2>100% MONEY BACK
+                    </h2>
+                    <h3>14 days return policy</h3>               
+                     </div>
+                     <div className="option">
+                     <i class="fa-solid fa-money-check-dollar"></i>
+                    <h2>PAYMENT SECURE
+                    </h2>
+                    <h3>Your data is protected</h3>               
+                     </div>
+            </div>
         </div>
     )
 }
