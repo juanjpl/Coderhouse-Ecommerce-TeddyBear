db1eda1175e5153341e00a6ffb0f9b18c916957b
Juan Lima <juan_jpl@hotmail.com>
2023-09-23 20:35:02 -0300

    add about page

--- src/views/AboutPage/AboutPage.jsx
+++ src/views/AboutPage/AboutPage.jsx
@@ -1,12 +1,34 @@
-import   './styles.css';
+import "./styles.css";
+import  imageAbout  from './assets/aboutusimage.jpg';
 
-
-const AboutPage =()=>{
-    return(
-        <div>
-            <h1>About Page</h1>
+const AboutPage = () => {
+  return (
+    <div className="container-about">
+      <div className="contenedor">
+        <div className="superior-bar">
+                <h2>About</h2>
         </div>
-    )
-}
+        <div className="content">
+          <h1>About us</h1>
+          <img className="image-about" src={imageAbout} alt="imagen about" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus labore omnis reiciendis aperiam! Quia cum accusantium, voluptatem facilis quasi modi eius consectetur totam eveniet quam veniam vel ducimus culpa eligendi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione laudantium iure aspernatur molestiae. Molestiae minus ad praesentium ea culpa quae dicta debitis temporibus hic! Reiciendis distinctio illo accusamus enim laudantium.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus labore omnis reiciendis aperiam! Quia cum accusantium, voluptatem facilis quasi modi eius consectetur totam eveniet quam veniam vel ducimus culpa eligendi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione laudantium iure aspernatur molestiae. Molestiae minus ad praesentium ea culpa quae dicta debitis temporibus hic! Reiciendis distinctio illo accusamus enim laudantium.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus labore omnis reiciendis aperiam! Quia cum accusantium, voluptatem facilis quasi modi eius consectetur totam eveniet quam veniam vel ducimus culpa eligendi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione laudantium iure aspernatur molestiae. Molestiae minus ad praesentium ea culpa quae dicta debitis temporibus hic! Reiciendis distinctio illo accusamus enim laudantium.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus labore omnis reiciendis aperiam! Quia cum accusantium, voluptatem facilis quasi modi eius consectetur totam eveniet quam veniam vel ducimus culpa eligendi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione laudantium iure aspernatur molestiae. Molestiae minus ad praesentium ea culpa quae dicta debitis temporibus hic! Reiciendis distinctio illo accusamus enim laudantium.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus labore omnis reiciendis aperiam! Quia cum accusantium, voluptatem facilis quasi modi eius consectetur totam eveniet quam veniam vel ducimus culpa eligendi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione laudantium iure aspernatur molestiae. Molestiae minus ad praesentium ea culpa quae dicta debitis temporibus hic! Reiciendis distinctio illo accusamus enim laudantium.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus labore omnis reiciendis aperiam! Quia cum accusantium, voluptatem facilis quasi modi eius consectetur totam eveniet quam veniam vel ducimus culpa eligendi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione laudantium iure aspernatur molestiae. Molestiae minus ad praesentium ea culpa quae dicta debitis temporibus hic! Reiciendis distinctio illo accusamus enim laudantium.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus labore omnis reiciendis aperiam! Quia cum accusantium, voluptatem facilis quasi modi eius consectetur totam eveniet quam veniam vel ducimus culpa eligendi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione laudantium iure aspernatur molestiae. Molestiae minus ad praesentium ea culpa quae dicta debitis temporibus hic! Reiciendis distinctio illo accusamus enim laudantium.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus labore omnis reiciendis aperiam! Quia cum accusantium, voluptatem facilis quasi modi eius consectetur totam eveniet quam veniam vel ducimus culpa eligendi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione laudantium iure aspernatur molestiae. Molestiae minus ad praesentium ea culpa quae dicta debitis temporibus hic! Reiciendis distinctio illo accusamus enim laudantium.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus labore omnis reiciendis aperiam! Quia cum accusantium, voluptatem facilis quasi modi eius consectetur totam eveniet quam veniam vel ducimus culpa eligendi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione laudantium iure aspernatur molestiae. Molestiae minus ad praesentium ea culpa quae dicta debitis temporibus hic! Reiciendis distinctio illo accusamus enim laudantium.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus labore omnis reiciendis aperiam! Quia cum accusantium, voluptatem facilis quasi modi eius consectetur totam eveniet quam veniam vel ducimus culpa eligendi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione laudantium iure aspernatur molestiae. Molestiae minus ad praesentium ea culpa quae dicta debitis temporibus hic! Reiciendis distinctio illo accusamus enim laudantium.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus labore omnis reiciendis aperiam! Quia cum accusantium, voluptatem facilis quasi modi eius consectetur totam eveniet quam veniam vel ducimus culpa eligendi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione laudantium iure aspernatur molestiae. Molestiae minus ad praesentium ea culpa quae dicta debitis temporibus hic! Reiciendis distinctio illo accusamus enim laudantium.
+
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AboutPage;
